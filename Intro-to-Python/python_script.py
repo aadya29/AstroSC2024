2dfd49c946f28c33ff42eda80de9dfeec6a9d7d5
Janiris Rodriguez <janirisrodriguez27@gmail.com>
2024-07-29 08:04:05 -0500

    adding files for intro to python and for transient ID project

---
+++ Intro-to-Python/python_script.py
@@ -0,0 +1,11 @@
+# print statement
+print('You\'re running your first Python script! Yippee!')
+
+#can do multiple things at once so we're also going to do some math
+x = 10
+y = 5
+print('x =', x)
+print('y =', y)
+print('x * y = ',x*y)
+print('y - x = ',y-x)
+print('The end!')
